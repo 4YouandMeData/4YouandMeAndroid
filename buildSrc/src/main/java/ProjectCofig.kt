d5700f9ec27d6113e225a011de277fc52058e07c
Giacomo Parisi <giacomo@balzo.eu>
2021-08-24 12:21:48 +0200

    Release 0.2.11

--- buildSrc/src/main/java/ProjectCofig.kt
+++ buildSrc/src/main/java/ProjectCofig.kt
@@ -3,8 +3,8 @@ object ProjectConfig {
     const val min_sdk = 23
     const val target_sdk = 30
     const val compile_sdk = 30
-    const val version_code = 61
-    const val version_name = "0.2.10"
+    const val version_code = 62
+    const val version_name = "0.2.11"
 
     const val test_instrumentation_runner = "androidx.test.runner.AndroidJUnitRunner"
 
