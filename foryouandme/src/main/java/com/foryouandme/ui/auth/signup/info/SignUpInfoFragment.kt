cf6761b7ae3dd460d1f947986fddc3299c88a402
Giacomo Parisi <giacomo@balzo.eu>
2021-09-02 14:14:15 +0200

    Refactor sign up info

--- foryouandme/src/main/java/com/foryouandme/ui/auth/signup/info/SignUpInfoFragment.kt
+++ foryouandme/src/main/java/com/foryouandme/ui/auth/signup/info/SignUpInfoFragment.kt
@@ -1,118 +1,36 @@
 package com.foryouandme.ui.auth.signup.info
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.viewModels
-import com.foryouandme.R
-import com.foryouandme.core.ext.html.setHtmlText
-import com.foryouandme.core.ext.setStatusBar
-import com.foryouandme.core.ext.showBackButton
-import com.foryouandme.databinding.SignUpInfoBinding
-import com.foryouandme.entity.configuration.HEXGradient
 import com.foryouandme.ui.auth.AuthSectionFragment
+import com.foryouandme.ui.auth.signup.info.compose.SignUpInfoPage
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SignUpInfoFragment : AuthSectionFragment(R.layout.sign_up_info) {
+class SignUpInfoFragment : AuthSectionFragment() {
 
     private val viewModel: SignUpInfoViewModel by viewModels()
 
-    private val binding: SignUpInfoBinding?
-        get() = view?.let { SignUpInfoBinding.bind(it) }
-
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        setupView()
-        applyConfiguration()
-
-    }
-
-    override fun onConfigurationChange() {
-        super.onConfigurationChange()
-
-        applyConfiguration()
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        viewModel.execute(SignUpInfoStateEvent.ScreenViewed)
-
-    }
-
-    private fun setupView() {
-
-        val viewBinding = binding
-
-        if (viewBinding != null) {
-
-            viewBinding.logo.setImageResource(imageConfiguration.logo())
-
-            viewBinding.signUp.setImageResource(imageConfiguration.nextStep())
-
-            viewBinding.signUpLater.setImageResource(imageConfiguration.nextStep())
-            viewBinding.signUpLater.setOnClickListener {
-                navigator.navigateTo(authNavController(), SignUpInfoToSignUpLater)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View =
+        ComposeView(requireContext()).apply {
+            setContent {
+                SignUpInfoPage(
+                    viewModel = viewModel,
+                    onBack = { back() },
+                    signIn = { navigator.navigateTo(authNavController(), it) },
+                    signUpLater = {
+                        navigator.navigateTo(authNavController(), SignUpInfoToSignUpLater)
+                    }
+                )
             }
-
-            viewBinding.signUpLaterText.setOnClickListener {
-                navigator.navigateTo(authNavController(), SignUpInfoToSignUpLater)
-            }
-
-            viewBinding.toolbar.showBackButton(imageConfiguration) { back() }
-
         }
-
-    }
-
-    private fun applyConfiguration() {
-
-        val viewBinding = binding
-        val config = configuration
-
-        if (viewBinding != null && config != null) {
-
-            setStatusBar(config.theme.primaryColorStart.color())
-
-            viewBinding.root.background =
-                HEXGradient.from(
-                    config.theme.primaryColorStart,
-                    config.theme.primaryColorEnd
-                ).drawable()
-
-            viewBinding.title.setTextColor(config.theme.secondaryColor.color())
-            viewBinding.title.setHtmlText(config.text.intro.title, true)
-
-            viewBinding.description.setTextColor(config.theme.secondaryColor.color())
-            viewBinding.description.setHtmlText(config.text.intro.body, true)
-
-            viewBinding.divider.setBackgroundColor(config.theme.primaryColorEnd.color())
-
-            viewBinding.signUpText.setTextColor(config.theme.secondaryColor.color())
-            viewBinding.signUpText.text = config.text.intro.login
-
-            viewBinding.signUpLaterText.setTextColor(config.theme.secondaryColor.color())
-            viewBinding.signUpLaterText.text = config.text.intro.back
-
-            viewBinding.signUp.setOnClickListener {
-                if (config.pinCodeLogin)
-                    navigator.navigateTo(authNavController(), SignUpInfoToPinCode)
-                else
-                    navigator.navigateTo(authNavController(), SignUpInfoToEnterPhone)
-            }
-
-            viewBinding.signUpText.setOnClickListener {
-                if (config.pinCodeLogin)
-                    navigator.navigateTo(authNavController(), SignUpInfoToPinCode)
-                else
-                    navigator.navigateTo(authNavController(), SignUpInfoToEnterPhone)
-            }
-
-        }
-
-    }
 
 }
--- foryouandme/src/main/java/com/foryouandme/ui/auth/signup/info/SignUpInfoViewModel.kt
+++ foryouandme/src/main/java/com/foryouandme/ui/auth/signup/info/SignUpInfoViewModel.kt
@@ -2,22 +2,51 @@ package com.foryouandme.ui.auth.signup.info
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.foryouandme.core.arch.flow.ErrorFlow
-import com.foryouandme.core.arch.flow.LoadingFlow
-import com.foryouandme.core.arch.flow.StateUpdateFlow
+import com.foryouandme.core.arch.deps.ImageConfiguration
+import com.foryouandme.core.arch.toData
+import com.foryouandme.core.arch.toError
+import com.foryouandme.core.ext.Action
+import com.foryouandme.core.ext.action
+import com.foryouandme.core.ext.launchAction
 import com.foryouandme.core.ext.launchSafe
 import com.foryouandme.domain.policy.Policy
 import com.foryouandme.domain.usecase.analytics.AnalyticsEvent
 import com.foryouandme.domain.usecase.analytics.EAnalyticsProvider
 import com.foryouandme.domain.usecase.analytics.SendAnalyticsEventUseCase
 import com.foryouandme.domain.usecase.configuration.GetConfigurationUseCase
+import com.foryouandme.ui.dailysurveytime.DailySurveyTimeAction
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class SignUpInfoViewModel @Inject constructor(
-    private val sendAnalyticsEventUseCase: SendAnalyticsEventUseCase
+    private val getConfigurationUseCase: GetConfigurationUseCase,
+    private val sendAnalyticsEventUseCase: SendAnalyticsEventUseCase,
+    val imageConfiguration: ImageConfiguration
 ) : ViewModel() {
+
+    /* --- state --- */
+
+    private val state = MutableStateFlow(SignUpInfoState())
+    val stateFlow = state as StateFlow<SignUpInfoState>
+
+    init {
+        execute(SignUpInfoAction.GetConfiguration)
+    }
+
+    /* --- configuration --- */
+
+    private fun getConfiguration(): Action =
+        action(
+            {
+                state.emit(state.value.copy(configuration = state.value.configuration.toLoading()))
+                val configuration = getConfigurationUseCase(Policy.LocalFirst)
+                state.emit(state.value.copy(configuration = configuration.toData()))
+            },
+            { state.emit(state.value.copy(configuration = it.toError())) }
+        )
 
     /* --- analytics --- */
 
@@ -28,11 +57,13 @@ class SignUpInfoViewModel @Inject constructor(
         )
     }
 
-    /* --- state event --- */
+    /* --- action --- */
 
-    fun execute(stateEvent: SignUpInfoStateEvent) {
-        when (stateEvent) {
-            SignUpInfoStateEvent.ScreenViewed ->
+    fun execute(action: SignUpInfoAction) {
+        when (action) {
+            SignUpInfoAction.GetConfiguration ->
+                viewModelScope.launchAction(getConfiguration())
+            SignUpInfoAction.ScreenViewed ->
                 viewModelScope.launchSafe { logScreenViewed() }
         }
 
