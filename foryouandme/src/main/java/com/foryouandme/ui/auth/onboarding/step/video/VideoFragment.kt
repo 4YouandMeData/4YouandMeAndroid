b6177c3150b68ef3b312efb61f1b6b4d7719b2e5
Giacomo Parisi <giacomo@balzo.eu>
2021-09-02 17:44:57 +0200

    Refactor video

--- foryouandme/src/main/java/com/foryouandme/ui/auth/onboarding/step/video/VideoFragment.kt
+++ foryouandme/src/main/java/com/foryouandme/ui/auth/onboarding/step/video/VideoFragment.kt
@@ -1,174 +1,34 @@
 package com.foryouandme.ui.auth.onboarding.step.video
 
-import android.content.pm.ActivityInfo
-import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.ImageView
-import androidx.core.view.isVisible
-import com.foryouandme.R
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.viewModels
 import com.foryouandme.core.arch.deps.VideoConfiguration
 import com.foryouandme.core.ext.*
 import com.foryouandme.databinding.VideoBinding
-import com.foryouandme.entity.configuration.Configuration
-import com.foryouandme.entity.configuration.button.button
 import com.foryouandme.ui.auth.onboarding.step.OnboardingStepFragment
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.Player
+import com.foryouandme.ui.auth.onboarding.step.video.compose.Video
 import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.source.MediaSource
-import com.google.android.exoplayer2.source.ProgressiveMediaSource
-import com.google.android.exoplayer2.upstream.DataSpec
-import com.google.android.exoplayer2.upstream.RawResourceDataSource
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class VideoFragment : OnboardingStepFragment(R.layout.video) {
+class VideoFragment : OnboardingStepFragment() {
 
-    @Inject
-    lateinit var videoConfiguration: VideoConfiguration
+    private val viewModel by viewModels<VideoViewModel>()
 
-    lateinit var player: SimpleExoPlayer
-
-    private val binding: VideoBinding?
-        get() = view?.let { VideoBinding.bind(it) }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        setupVideo()
-
-        if (requireActivity().requestedOrientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
-            requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
-
-        applyConfiguration()
-
-    }
-
-    override fun onConfigurationChange() {
-        super.onConfigurationChange()
-        applyConfiguration()
-    }
-
-    private fun setupVideo() {
-
-        val viewBinding = binding
-
-        if (viewBinding != null) {
-
-            player = SimpleExoPlayer.Builder(requireContext()).build()
-
-            val media = buildRawMediaSource()
-
-            media.let {
-                player.setMediaSource(it)
-                player.prepare()
-                player.playWhenReady = false
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View =
+        ComposeView(requireContext()).apply {
+            setContent {
+                Video(viewModel = viewModel, next = { next() })
             }
-
-            viewBinding.video.player = player
-
-            viewBinding.fakePlay.setOnClickListener {
-
-                viewBinding.fakePlay.isVisible = false
-                viewBinding.next.isVisible = false
-                viewBinding.video.useController = true
-                viewBinding.video.showController()
-                player.play()
-
-            }
-
-            player.addListener(object : Player.EventListener {
-
-                override fun onPlaybackStateChanged(state: Int) {
-                    super.onPlaybackStateChanged(state)
-
-                    when (state) {
-
-                        Player.STATE_ENDED -> next()
-
-                        else -> {
-                        }
-
-                    }
-                }
-
-            })
-
         }
-    }
-
-    private fun applyConfiguration() {
-
-        val config = configuration
-        val viewBinding = binding
-
-        if (config != null && viewBinding != null) {
-
-            setStatusBar(Color.BLACK)
-
-            viewBinding.toolbar.showCloseSecondaryButton(imageConfiguration) { next() }
-
-            viewBinding.fakePlay.setImageResource(imageConfiguration.videoDiaryPlay())
-
-            viewBinding.root.findViewById<ImageView>(R.id.exo_play)
-                .setImageResource(imageConfiguration.videoDiaryPlay())
-
-            viewBinding.root.findViewById<ImageView>(R.id.exo_pause)
-                .setImageResource(imageConfiguration.videoDiaryPause())
-
-            viewBinding.next.text = config.text.onboarding.introVideoContinueButton
-            viewBinding.next.setTextColor(config.theme.secondaryColor.color())
-            viewBinding.next.background = button(config.theme.primaryColorEnd.color())
-
-            viewBinding.next.setOnClickListener { next() }
-
-        }
-    }
-
-    private fun buildRawMediaSource(): MediaSource {
-
-        val rawDataSource = RawResourceDataSource(requireContext())
-        // open the /raw resource file
-        rawDataSource.open(
-            DataSpec(
-                RawResourceDataSource.buildRawResourceUri(
-                    videoConfiguration.introVideo()
-                )
-            )
-        )
-
-        // Create media Item
-        val mediaItem = MediaItem.fromUri(rawDataSource.uri!!)
-
-        // create a media source with the raw DataSource
-        return ProgressiveMediaSource.Factory { rawDataSource }.createMediaSource(mediaItem)
-
-    }
-
-    override fun onPause() {
-
-        player.pause()
-
-        super.onPause()
-
-    }
-
-    override fun onDestroyView() {
-
-        requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-
-        super.onDestroyView()
-
-    }
-
-    override fun onDestroy() {
-
-        player.release()
-
-        super.onDestroy()
-
-    }
 
 }
--- foryouandme/src/main/java/com/foryouandme/ui/compose/video/VideoPlayer.kt
+++ foryouandme/src/main/java/com/foryouandme/ui/compose/video/VideoPlayer.kt
@@ -1,6 +1,8 @@
 package com.foryouandme.ui.compose.video
 
 import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
 import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -18,11 +20,138 @@ import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DataSpec
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.upstream.RawResourceDataSource
 import com.google.android.exoplayer2.util.Util
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
+
+@Composable
+fun VideoPlayer(
+    resource: Int,
+    modifier: Modifier = Modifier,
+    isPlaying: Boolean = false,
+    usePlayerController: Boolean = false,
+    loop: Boolean = false,
+    playerLayoutId: Int? = null,
+    playerResizeMode: Int? = AspectRatioFrameLayout.RESIZE_MODE_ZOOM,
+    configurePlayer: (View) -> Unit = {},
+    onEnd: () -> Unit = {}
+) {
+
+    val context = LocalContext.current
+    val mediaSource = remember {
+
+        val rawDataSource = RawResourceDataSource(context)
+        // open the /raw resource file
+        rawDataSource.open(DataSpec(RawResourceDataSource.buildRawResourceUri(resource)))
+
+        // Create media Item
+        val mediaItem = MediaItem.fromUri(rawDataSource.uri!!)
+
+        // create a media source with the raw DataSource
+        ProgressiveMediaSource.Factory { rawDataSource }.createMediaSource(mediaItem)
+    }
+
+    VideoPlayer(
+        mediaSource = mediaSource,
+        isPlaying = isPlaying,
+        usePlayerController = usePlayerController,
+        loop = loop,
+        playerLayoutId = playerLayoutId,
+        playerResizeMode = playerResizeMode,
+        configurePlayer = configurePlayer,
+        onEnd = onEnd,
+        modifier = modifier
+    )
+
+}
+
+@Composable
+fun VideoPlayer(
+    mediaSource: ProgressiveMediaSource,
+    modifier: Modifier = Modifier,
+    isPlaying: Boolean = false,
+    usePlayerController: Boolean = false,
+    playerLayoutId: Int? = null,
+    playerResizeMode: Int? = AspectRatioFrameLayout.RESIZE_MODE_ZOOM,
+    loop: Boolean = false,
+    configurePlayer: (View) -> Unit = {},
+    onEnd: () -> Unit = {}
+) {
+
+    val context = LocalContext.current
+
+    // Do not recreate the player everytime this Composable commits
+    val exoPlayer = remember { SimpleExoPlayer.Builder(context).build() }
+
+    LaunchedEffect(mediaSource) {
+
+        exoPlayer.setMediaSource(mediaSource)
+        if (loop) exoPlayer.repeatMode = Player.REPEAT_MODE_ALL
+        exoPlayer.prepare()
+
+    }
+
+    LaunchedEffect(key1 = isPlaying) {
+        if (isPlaying != exoPlayer.isPlaying) {
+            if (isPlaying) exoPlayer.play() else exoPlayer.pause()
+        }
+    }
+
+    AndroidView(
+        factory = { ctx ->
+            val view =
+                if (playerLayoutId != null)
+                    LayoutInflater.from(ctx).inflate(
+                        playerLayoutId,
+                        null,
+                        false
+                    ) as PlayerView
+                else
+                    PlayerView(ctx)
+            view.apply {
+                player = exoPlayer
+                if (playerResizeMode != null) resizeMode = playerResizeMode
+                useController = usePlayerController
+                exoPlayer.addListener(object : Player.EventListener {
+
+                    override fun onPlaybackStateChanged(state: Int) {
+                        super.onPlaybackStateChanged(state)
+
+                        when (state) {
+
+                            Player.STATE_ENDED -> onEnd()
+                            else -> Unit
+
+                        }
+                    }
+
+                })
+                configurePlayer(this)
+            }
+
+            view
+        },
+        update = {
+            if (it.useController != usePlayerController)
+                it.useController = usePlayerController
+        },
+        modifier = modifier.background(Color.Black)
+    )
+
+    DisposableEffect(key1 = "video_player") {
+
+        onDispose {
+            catchToNull { exoPlayer.stop() }
+            catchToNull { exoPlayer.release() }
+        }
+
+    }
+
+}
 
 @Composable
 fun VideoPlayer(
