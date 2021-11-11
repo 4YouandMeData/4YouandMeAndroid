68f30adff1dce06cc7595a138ef0a962b9838573
Giacomo Parisi <giacomo@balzo.eu>
2021-11-11 10:50:06 +0100

    Release 0.2.16

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -3,8 +3,8 @@ object ProjectConfig {
     const val min_sdk = 23
     const val target_sdk = 30
     const val compile_sdk = 31
-    const val version_code = 68
-    const val version_name = "0.2.15"
+    const val version_code = 69
+    const val version_name = "0.2.16"
 
     const val test_instrumentation_runner = "androidx.test.runner.AndroidJUnitRunner"
 
