d4109365f0dbc419b643f42542a9bd5e0abd4dcb
Iacopo Checcacci <iacopo@balzo.eu>
2021-12-16 17:48:13 +0100

    release version 0.2.19(72)

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -3,8 +3,8 @@ object ProjectConfig {
     const val min_sdk = 23
     const val target_sdk = 30
     const val compile_sdk = 31
-    const val version_code = 70
-    const val version_name = "0.2.17"
+    const val version_code = 72
+    const val version_name = "0.2.19"
 
     const val test_instrumentation_runner = "androidx.test.runner.AndroidJUnitRunner"
 
