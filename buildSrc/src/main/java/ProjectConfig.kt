fb5a10ab6559a66fcbcb1054a1bc4a70f6de07cf
Giacomo Parisi <giacomo@balzo.eu>
2022-04-21 11:22:50 +0200

    Release 0.2.24

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -3,8 +3,8 @@ object ProjectConfig {
     const val min_sdk = 23
     const val target_sdk = 30
     const val compile_sdk = 31
-    const val version_code = 76
-    const val version_name = "0.2.23"
+    const val version_code = 77
+    const val version_name = "0.2.24"
 
     const val test_instrumentation_runner = "androidx.test.runner.AndroidJUnitRunner"
 
