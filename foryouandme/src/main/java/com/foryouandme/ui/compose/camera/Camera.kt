1142793326dedacbe1fae07875323480a505fc19
Gianluca Galloni <gianluca.galloni@stride-it.com>
2023-02-13 20:29:44 +0100

    Added support for filter activation and deactivation. ToDo: button for filter control

--- foryouandme/src/main/java/com/foryouandme/ui/compose/camera/Camera.kt
+++ foryouandme/src/main/java/com/foryouandme/ui/compose/camera/Camera.kt
@@ -2,12 +2,10 @@ package com.foryouandme.ui.compose.camera
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.pm.ApplicationInfo
-import android.content.pm.PackageManager
 import android.graphics.SurfaceTexture
+import android.util.Log
 import android.util.Rational
 import android.util.Size
-import android.view.Surface
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.view.View
@@ -17,7 +15,6 @@ import androidx.camera.core.Preview
 import androidx.camera.core.VideoCapture
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
@@ -25,13 +22,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import com.foryouandme.core.ext.catchToNull
 import com.foryouandme.core.ext.findActivity
 import com.google.mediapipe.components.CameraHelper
+import com.google.mediapipe.components.CameraHelper.CameraFacing
 import com.google.mediapipe.components.CameraXPreviewHelper
 import com.google.mediapipe.components.ExternalTextureConverter
 import com.google.mediapipe.components.FrameProcessor
@@ -59,127 +56,101 @@ fun Camera(
     var camera: Camera? by remember { mutableStateOf(null) }
     var currentLens by remember { mutableStateOf(cameraLens) }
     var currentFlash by remember { mutableStateOf(cameraFlash) }
-    var processor: FrameProcessor?
-    var eglManager: EglManager? = null
-    var previewFrameTexture: SurfaceTexture? = null
-    var surfaceView: SurfaceView?
-    var converter: ExternalTextureConverter?
-    var cameraHelper: CameraXPreviewHelper? = null
+    var isFilterActive = true
+    var surfaceView: SurfaceView
 
     BoxWithConstraints(modifier = modifier) {
 
         val widthPixel = LocalDensity.current.run { maxWidth.toPx() }
         val heightPixel = LocalDensity.current.run { maxHeight.toPx() }
 
-        AndroidView(
-            factory = { ctx ->
-                val previewView = PreviewView(ctx)
 
-                surfaceView = SurfaceView(ctx)
+        if(!isFilterActive) {
+            AndroidView(
+                factory = { ctx ->
+                    val previewView = PreviewView(ctx)
 
-                AndroidAssetUtil.initializeNativeAssetManager(ctx);
-                if (eglManager == null) {
-                    eglManager = EglManager(null)
-                }
+                    previewView.scaleType = PreviewView.ScaleType.FILL_CENTER
+                    previewView.implementationMode = PreviewView.ImplementationMode.COMPATIBLE
 
-                processor = FrameProcessor(
-                    ctx,
-                    eglManager!!.nativeContext,
-                    "mobile_gpu.binarypb",
-                    "input_video",
-                    "output_video"
-                )
-
-                processor!!
-                    .videoSurfaceOutput
-                    .setFlipY(
-                        true
-                    )
-
-                converter = ExternalTextureConverter(eglManager!!.context)
-                converter!!.setFlipY(true)
-                converter!!.setConsumer(processor)
-
-                //Set Preview Display
-                surfaceView!!.visibility = View.GONE
-                surfaceView!!
-                    .holder
-                    .addCallback(
-                        object : SurfaceHolder.Callback {
-                            override fun surfaceCreated(holder: SurfaceHolder) {
-                                processor!!.videoSurfaceOutput.setSurface(holder.surface)
-                            }
-
-                            override fun surfaceChanged(
-                                holder: SurfaceHolder,
-                                format: Int,
-                                width: Int,
-                                height: Int
-                            ) {
-                                val viewSize = Size(width, height)
-                                val displaySize =
-                                    cameraHelper?.computeDisplaySizeFromViewSize(viewSize)
-
-                                converter!!.setSurfaceTextureAndAttachToGLContext(
-                                    previewFrameTexture, displaySize!!.width, displaySize.height
-                                )
-                            }
-
-                            override fun surfaceDestroyed(holder: SurfaceHolder) {
-                                processor!!.videoSurfaceOutput.setSurface(null)
-                            }
-                        })
-
-                // Start Camera
-                cameraHelper = CameraXPreviewHelper()
-                cameraHelper!!.setOnCameraStartedListener{surfaceTexture: SurfaceTexture? ->
-                    previewFrameTexture = surfaceTexture
-                    surfaceView!!.visibility = View.VISIBLE
-                }
-                cameraHelper?.startCamera(context.findActivity(), CameraHelper.CameraFacing.FRONT, null)
-
-                //Old code
-                previewView.scaleType = PreviewView.ScaleType.FILL_CENTER
-                previewView.implementationMode = PreviewView.ImplementationMode.COMPATIBLE
-                /*  startCamera(
-                      context,
-                      lifecycleOwner,
-                      previewView,
-                      videoCapture,
-                      cameraLens,
-                      widthPixel,
-                      heightPixel,
-                  )
-                  {
-                      camera = it
-                      currentLens = cameraLens
-                  }*/
-                surfaceView!!
-            },
-            modifier = Modifier.fillMaxSize(),
-            update = { view ->
-                if (cameraLens != currentLens)
-                    /*startCamera(
-                        context,
+                    startCamera(
+                        ctx,
                         lifecycleOwner,
-                        view,
+                        previewView,
                         videoCapture,
                         cameraLens,
                         widthPixel,
-                        heightPixel
+                        heightPixel,
                     )
                     {
                         camera = it
                         currentLens = cameraLens
-                    }*/
+                    }
+                    previewView
+                },
+                modifier = Modifier.fillMaxSize(),
+                update = { view ->
+                    if (cameraLens != currentLens)
+                        startCamera(
+                            context,
+                            lifecycleOwner,
+                            view,
+                            videoCapture,
+                            cameraLens,
+                            widthPixel,
+                            heightPixel,
+                        )
+                        {
+                            camera = it
+                            currentLens = cameraLens
+                        }
 
-                if (cameraFlash != currentFlash) {
-                    camera?.cameraControl?.enableTorch(cameraFlash is CameraFlash.On)
-                    currentFlash = cameraFlash
+                    if (cameraFlash != currentFlash) {
+                        camera?.cameraControl?.enableTorch(cameraFlash is CameraFlash.On)
+                        currentFlash = cameraFlash
+                    }
                 }
+            )
+        } else {
+            val cameraBack: String = "Back"
+            var currentCamera: CameraFacing = if (cameraLens.toString().contains(cameraBack)) CameraFacing.BACK else CameraFacing.FRONT
+            AndroidView(
+                factory = { ctx ->
+                    surfaceView = SurfaceView(ctx)
+                    startCameraFilter(
+                        ctx,
+                        surfaceView,
+                        currentCamera
+                    )
+                    surfaceView
+                },
+                modifier = Modifier.fillMaxSize(),
+                update = { view ->
+                    if (cameraLens != currentLens) {
+                        
+                        currentLens = cameraLens
+                        currentCamera = if(cameraLens.toString().contains(cameraBack)) {
+                            CameraFacing.BACK
+                        } else {
+                            CameraFacing.FRONT
+                        }
 
-            }
-        )
+                        startCameraFilter(
+                            context,
+                            view,
+                            currentCamera
+                        )
+
+                    }
+
+                    if (cameraFlash != currentFlash) {
+                        camera?.cameraControl?.enableTorch(cameraFlash is CameraFlash.On)
+                        currentFlash = cameraFlash
+                    }
+                }
+            )
+        }
+
     }
 
     LaunchedEffect(key1 = cameraEvents) {
@@ -216,8 +187,7 @@ fun Camera(
     }
 }
 
-/*
-fun startCamera(
+fun startCamera (
     context: Context,
     lifecycleOwner: LifecycleOwner,
     previewView: PreviewView,
@@ -227,44 +197,123 @@ fun startCamera(
     height: Float,
     onCameraReady: (Camera) -> Unit,
 ) {
-    val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
-    val executor = ContextCompat.getMainExecutor(context)
-    val aspectRatio = Rational(width.roundToInt(), height.roundToInt())
-    cameraProviderFuture.addListener(
-        {
-            val cameraProvider = cameraProviderFuture.get()
-            val preview =
-                Preview.Builder()
-                    .setTargetAspectRatio(aspectRatio.toInt())
-                    .build()
-                    .also { it.setSurfaceProvider(previewView.surfaceProvider) }
 
-            val cameraSelector =
-                CameraSelector.Builder()
-                    .requireLensFacing(
-                        when (lens) {
-                            CameraLens.Back -> CameraSelector.LENS_FACING_BACK
-                            CameraLens.Front -> CameraSelector.LENS_FACING_FRONT
-                        }
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+        val executor = ContextCompat.getMainExecutor(context)
+        val aspectRatio = Rational(width.roundToInt(), height.roundToInt())
+        cameraProviderFuture.addListener(
+            {
+                val cameraProvider = cameraProviderFuture.get()
+                val preview =
+                    Preview.Builder()
+                        .setTargetAspectRatio(aspectRatio.toInt())
+                        .build()
+                        .also { it.setSurfaceProvider(previewView.surfaceProvider) }
+
+                val cameraSelector =
+                    CameraSelector.Builder()
+                        .requireLensFacing(
+                            when (lens) {
+                                CameraLens.Back -> CameraSelector.LENS_FACING_BACK
+                                CameraLens.Front -> CameraSelector.LENS_FACING_FRONT
+                            }
+                        )
+                        .build()
+
+                catchToNull { cameraProvider.unbindAll() }
+                val camera =
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner,
+                        cameraSelector,
+                        preview,
+                        videoCapture
                     )
-                    .build()
 
-            catchToNull { cameraProvider.unbindAll() }
-            val camera =
-                cameraProvider.bindToLifecycle(
-                    lifecycleOwner,
-                    cameraSelector,
-                    preview,
-                    videoCapture
-                )
+                onCameraReady(camera)
 
-            onCameraReady(camera)
+            },
+            executor
+        )
 
-        },
-        executor
-    )
 }
-*/
+
+fun startCameraFilter(
+    context: Context,
+    surfaceView: SurfaceView,
+    cameraFacing: CameraFacing
+) {
+
+    ////////////////////////////// MediaPipe //////////////////////////////
+    var cameraHelper: CameraXPreviewHelper? = null
+    var previewFrameTexture: SurfaceTexture? = null
+    val eglManager: EglManager?
+    val processor: FrameProcessor?
+    val converter: ExternalTextureConverter?
+
+    AndroidAssetUtil.initializeNativeAssetManager(context);
+    eglManager = EglManager(null)
+
+    processor = FrameProcessor(
+        context,
+        eglManager.nativeContext,
+        "mobile_gpu.binarypb",
+        "input_video",
+        "output_video"
+    )
+
+    processor.videoSurfaceOutput.setSurface(null)
+
+    processor
+        .videoSurfaceOutput
+        .setFlipY(
+            true
+        )
+
+    converter = ExternalTextureConverter(eglManager.context)
+    converter.setFlipY(true)
+    converter.setConsumer(processor)
+
+    //Set Preview Display
+    surfaceView.visibility = View.GONE
+    surfaceView
+        .holder
+        .addCallback(
+            object : SurfaceHolder.Callback {
+                override fun surfaceCreated(holder: SurfaceHolder) {
+                    processor.videoSurfaceOutput.setSurface(holder.surface)
+                }
+
+                override fun surfaceChanged(
+                    holder: SurfaceHolder,
+                    format: Int,
+                    width: Int,
+                    height: Int
+                ) {
+                    val viewSize = Size(width, height)
+                    val displaySize =
+                        cameraHelper?.computeDisplaySizeFromViewSize(viewSize)
+
+                    converter.setSurfaceTextureAndAttachToGLContext(
+                        previewFrameTexture, displaySize!!.width, displaySize.height
+                    )
+                }
+
+                override fun surfaceDestroyed(holder: SurfaceHolder) {
+                    processor.videoSurfaceOutput.setSurface(null)
+                }
+            })
+    ////////////////////////////// MediaPipe //////////////////////////////
+
+
+    // Start Camera
+    cameraHelper = CameraXPreviewHelper()
+    cameraHelper.setOnCameraStartedListener{ surfaceTexture: SurfaceTexture? ->
+        previewFrameTexture = surfaceTexture
+        surfaceView.visibility = View.VISIBLE
+    }
+    cameraHelper.startCamera(context.findActivity(), cameraFacing, null)
+
+}
 
 @SuppressLint("RestrictedApi")
 private fun builtVideoCapture(settings: VideoSettings): VideoCapture {
